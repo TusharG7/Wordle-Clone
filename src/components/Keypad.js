6fb167f157619f5ac878eaaa103220f1b4cc0bfc
Tushar Gajwani <89583890+TusharG7@users.noreply.github.com>
2023-05-30 14:08:51 +0530

    local db fixes

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import db from "../db";
 
-export default function Keypad({usedKeys}) {
-  const [letters, setLetters] = useState(null)
+export default function Keypad({ usedKeys }) {
+  const [letters, setLetters] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/letters')
-      .then(res => res.json())
-      .then(json => {
-        setLetters(json)
-      })
-  }, [])
+    setLetters(db.letters);
+    // fetch('http://localhost:8000/letters')
+    //   .then(res => res.json())
+    //   .then(json => {
+    //     setLetters(json)
+    //   })
+  }, []);
 
   return (
     <div className="keypad">
-      {letters && letters.map(letter => {
-        const color = usedKeys[letter.key]
-        return (
-          <div key={letter.key} className={color}>{letter.key}</div>
-        )
-      })}
+      {letters &&
+        letters.map((letter) => {
+          const color = usedKeys[letter.key];
+          return (
+            <div key={letter.key} className={color}>
+              {letter.key}
+            </div>
+          );
+        })}
     </div>
-  )
+  );
 }
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,41 +1,44 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import useWordle from '../hooks/useWordle'
-import Grid from './Grid';
-import Keypad from './Keypad';
-import Modal from './Modal';
+import React from "react";
+import { useEffect, useState } from "react";
+import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
+import Keypad from "./Keypad";
+import Modal from "./Modal";
 
-export default function Wordle({solution}) {
-    const {currentGuess, handleKeyup, guesses, turn,usedKeys, isCorrect} = useWordle(solution);
-    const [showModal, setShowModal] = useState(false)
+export default function Wordle({ solution }) {
+  const { currentGuess, handleKeyup, guesses, turn, usedKeys, isCorrect } =
+    useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-      window.addEventListener('keyup',handleKeyup)
-      
-      if (isCorrect) {
-        setTimeout(() => {
-          setShowModal(true)
-        }, 2000);
-        window.removeEventListener('keyup',handleKeyup);
-      }
-      if (turn > 5) {
-        setTimeout(() => {
-          setShowModal(true)
-        }, 2000);
-        window.removeEventListener('keyup',handleKeyup);
-      }
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyup);
 
-      return () => {
-        window.removeEventListener('keyup',handleKeyup);
-      }
-    }, [handleKeyup, isCorrect, turn])
+    if (isCorrect) {
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+    if (turn > 5) {
+      setTimeout(() => {
+        setShowModal(true);
+      }, 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
+    return () => {
+      window.removeEventListener("keyup", handleKeyup);
+    };
+  }, [handleKeyup, isCorrect, turn]);
   return (
-    <div>
-    <div>solution : {solution}</div>
-    <div>current guess : {currentGuess}</div>
-    <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
-    <Keypad usedKeys={usedKeys}/>
-    {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution}/>}
-    </div>
-  )
+    <>
+      <h5>solution : {solution}</h5>
+      <h3>current guess : {currentGuess}</h3>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
+    </>
+  );
 }
